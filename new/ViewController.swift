6a0f0770e3ef095c32570d1a9da6827daf0f4d17
athai0493 <73952477+athai0493@users.noreply.github.com>
2021-06-29 13:43:24 -0400

    hey

--- new/ViewController.swift
+++ new/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitButton(_ sender: UIButton) {
+        textView.text = "You have a good taste in music!"
+        
+    }
+    
 }
 
